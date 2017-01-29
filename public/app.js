0c657cc1bb03eead5fb868d800049a45662a292d
Abi Auslander <abi920@gmail.com>
2017-01-29 00:19:44 -0500

    now able to add recipes to db

--- public/app.js
+++ public/app.js
@@ -2,6 +2,7 @@ var app = angular.module("FoodApp", []);
 
 
 app.controller('foodController', ['$http', function($http) {
+  this.recipeGroup = [];
 
   // function to pull all categories from rails API
   this.getCategories = function() {
@@ -21,6 +22,16 @@ app.controller('foodController', ['$http', function($http) {
   // Function to populate results from 2 APIs
   this.query = function() {
 
+    $http({
+      method: 'POST',
+      url: 'http://localhost:3000/categories',
+      data: {
+        name: this.searchTerm
+      }
+    }).then(function(response) {
+      console.log(response);
+      categoryId = response.data.id;
+    }.bind(this));
     // Yelp API call here
 
     // Food2Fork API call here
@@ -28,11 +39,32 @@ app.controller('foodController', ['$http', function($http) {
       method: 'GET',
       url: 'http://food2fork.com/api/search?key=' + this.key + '&q=' + this.searchTerm
     }).then(function(response) {
-      console.log(response.data);
-      console.log(response.data.recipes);
-      this.recipeData = response.data.recipes;
+      // console.log(response.data);
+      // console.log(response.data.recipes);
+      this.recipeGroup = response.data.recipes.map(function (recipeObj) {
+        var rObj = {};
+        rObj['name'] = recipeObj.title;
+        rObj['img_url'] = recipeObj.image_url;
+        rObj['url'] = recipeObj.source_url;
+        rObj['category_id'] = categoryId;
+        return rObj;
+      });
+      console.log(this.recipeGroup);
       // this returns a maximum of 30 recipes; could put in a shuffle function to shuffle the array and display in random order instead of same order every time?
     }.bind(this));
   };
+
+  // Function to add a recipe to the db:
+  this.addRecipe = function(index) {
+
+    $http({
+      method: 'POST',
+      url: 'http://localhost:3000/recipes',
+      data: { recipe: this.recipeGroup[index] }
+    }).then(function(response) {
+      console.log('adding recipe');
+      console.log(response);
+    }.bind(this));
+  }
 
 }]); // end app controller
