8aa56c76a82851003547be9d2a9fcbcf2308149f
Abi Auslander <abi920@gmail.com>
2017-01-28 21:19:52 -0500

    function to pull data from rails API is successful

--- public/app.js
+++ public/app.js
@@ -1,10 +1,29 @@
-var app = angular.module('FoodApp', []);
+var app = angular.module("FoodApp", []);
+
 
 app.controller('foodController', ['$http', function($http) {
+
+  // function to pull all categories from rails API
+  this.getCategories = function() {
+    $http({
+      method: 'GET',
+      url: 'http://localhost:3000/categories'
+    }).then(function(response) {
+      console.log(response);
+      console.log(response.data);
+      this.categories = response.data;
+    }.bind(this));
+  }
+
   this.key = '4674c93fb8691a3cc6c841773dc368e4';
   this.searchTerm = '';
 
+  // Function to populate results from 2 APIs
   this.query = function() {
+
+    // Yelp API call here
+
+    // Food2Fork API call here
     $http({
       method: 'GET',
       url: 'http://food2fork.com/api/search?key=' + this.key + '&q=' + this.searchTerm
