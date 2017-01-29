64f75adf7575b85b8dadd0bd178c2a6bb382daca
Shannon Matloob <smtlb90@gmail.com>
2017-01-29 15:55:54 -0500

    Initialize Yelp API

--- public/app.js
+++ public/app.js
@@ -3,6 +3,7 @@ var app = angular.module('FoodApp', []);
 app.controller('foodController', ['$http', function($http) {
   this.key = '4674c93fb8691a3cc6c841773dc368e4';
   this.searchTerm = '';
+  this.location = '';
 
   this.query = function() {
     $http({
@@ -13,6 +14,24 @@ app.controller('foodController', ['$http', function($http) {
       console.log(response.data.recipes);
       this.recipeData = response.data.recipes;
       // this returns a maximum of 30 recipes; could put in a shuffle function to shuffle the array and display in random order instead of same order every time?
+    }.bind(this));
+  };
+
+  this.placeQuery = function() {
+    $http({
+      method: 'GET',
+      url: "https://api.yelp.com/v3/businesses/search?term=" + this.searchTerm + "&location=" + this.location +  "&categories=restaurants&sort_by=distance",
+      headers: {
+        'authorization': 'Bearer kxeNhjKVOho4QTBNr0mSGwdtCKGZTmW-PgvVQpjMkrNIucjw3uN63VJWg8GxX6j4DsI4Xn1oaWKQO-HzJBFQJzu0-XUzIeNZqv8ghhER5m_IQFCs3NjWF3ST6QWOWHYx',
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Headers' : '*',
+        'Access-Control-Allow-Methods' : 'OPTIONS'
+      },
+      xsrfHeaderName : ""
+    }).then(function(response) {
+      console.log(response.data);
+      console.log(response.data.places);
+      this.placeData = response.data.places;
     }.bind(this));
   };
 
