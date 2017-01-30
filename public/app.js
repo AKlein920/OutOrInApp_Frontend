ab47f77b541d846b18d7fbe65be5305a1665dfd2
Abi Auslander <abi920@gmail.com>
2017-01-30 00:10:11 -0500

    properly adding recipes by category id; using modals to display all recipes associated with a category

--- public/app.js
+++ public/app.js
@@ -1,96 +1,106 @@
 var app = angular.module("FoodApp", []);
 
-
 app.controller('foodController', ['$http', function($http) {
   this.recipeGroup = [];
+  this.categories = [];
+  this.categoryRecipes = [];
 
-  // function to pull all categories from rails API
-  this.getCategories = function() {
+  // function to pull all categories from rails API:
     $http({
       method: 'GET',
       url: 'http://localhost:3000/categories'
     }).then(function(response) {
-      console.log(response);
+      // console.log(response);
       console.log(response.data);
       this.categories = response.data;
+    }.bind(this));
+
+  // function to show all of that category's recipes:
+  this.showRecipesByCategory = function(index) {
+    $http({
+      method: 'GET',
+      url: 'http://localhost:3000/categories/' + this.categories[index].id + '/recipes'
+    }).then(function(response) {
+      console.log(response.data);
+      this.categoryRecipes = response.data;
     }.bind(this));
   }
 
   this.key = '4674c93fb8691a3cc6c841773dc368e4';
   this.searchTerm = '';
-  this.location = '';
 
   // Function to populate results from 2 APIs
   this.query = function() {
+    this.searchTerm = this.searchTerm.toLowerCase();
 
-    // check if category list includes current search term:
+    // get all categories:
     $http({
       method: 'GET',
       url: 'http://localhost:3000/categories'
     }).then(function(response) {
-      console.log(response.data);
       this.allCategories = response.data;
-      this.allCategories.sort();
-      for (var i = 0; i < this.allCategories.length; i++) {
-        if (this.allCategories[i] == this.searchTerm) {
-          return this.allCategories[i].category_id;
+    }.bind(this));
+
+    // check if category list already includes current search term:
+    // for (var i = 0; i < allCategories.length; i++) {
+    //   if (allCategories[i].name == this.searchTerm) {
+    //     this.currentCategoryId = allCategories[i].id;
+    //   }
+    // }
+
+    // add current searchTerm to category list and grab its id:
+      $http({
+        method: 'POST',
+        url: 'http://localhost:3000/categories',
+        data: {
+          name: this.searchTerm
         }
-      }
-    }.bind(this));
+        }).then(function(response) {
+          console.log(response);
+          categoryId = response.data.id;
+        }.bind(this));
+        // get recipes from API:
+        $http({
+          method: 'GET',
+          url: 'http://food2fork.com/api/search?key=' + this.key + '&q=' + this.searchTerm
+        }).then(function(response) {
+          // console.log(response.data);
+          // console.log(response.data.recipes);
+          this.recipeGroup = response.data.recipes.map(function (recipeObj) {
+            var rObj = {};
+            rObj['name'] = recipeObj.title;
+            rObj['img_url'] = recipeObj.image_url;
+            rObj['url'] = recipeObj.source_url;
+            rObj['category_id'] = categoryId;
+            return rObj;
+          });
+          // console.log(this.recipeGroup);
+          // this returns a maximum of 30 recipes; could put in a shuffle function to shuffle the array and display in random order instead of same order every time?
+        }.bind(this));
+      } /////////// END this.query function
 
-    // add current search term to category list
-    $http({
-      method: 'POST',
-      url: 'http://localhost:3000/categories',
-      data: {
-        name: this.searchTerm
-      }
-    }).then(function(response) {
-      console.log(response);
-      categoryId = response.data.id;
-    }.bind(this));
+
     // Yelp API call here
-    $http({
-      method: 'GET',
-      url: "https://api.yelp.com/v3/businesses/search?term=" + this.searchTerm + "&location=" + this.location +  "&categories=restaurants&sort_by=distance",
-      headers: {
-        'authorization': 'Bearer kxeNhjKVOho4QTBNr0mSGwdtCKGZTmW-PgvVQpjMkrNIucjw3uN63VJWg8GxX6j4DsI4Xn1oaWKQO-HzJBFQJzu0-XUzIeNZqv8ghhER5m_IQFCs3NjWF3ST6QWOWHYx',
-        'Access-Control-Allow-Origin' : '*',
-        'Access-Control-Allow-Headers' : '*',
-        'Access-Control-Allow-Methods' : 'OPTIONS'
-      },
-    }).then(function(response) {
-      console.log(response.data);
-      console.log(response.data.places);
-      this.placeData = response.data.places;
-    }.bind(this));
-
-    // Food2Fork API call here
-    $http({
-      method: 'GET',
-      url: 'http://food2fork.com/api/search?key=' + this.key + '&q=' + this.searchTerm
-    }).then(function(response) {
-      // console.log(response.data);
-      // console.log(response.data.recipes);
-      this.recipeGroup = response.data.recipes.map(function (recipeObj) {
-        var rObj = {};
-        rObj['name'] = recipeObj.title;
-        rObj['img_url'] = recipeObj.image_url;
-        rObj['url'] = recipeObj.source_url;
-        rObj['category_id'] = categoryId;
-        return rObj;
-      });
-      console.log(this.recipeGroup);
-      // this returns a maximum of 30 recipes; could put in a shuffle function to shuffle the array and display in random order instead of same order every time?
-    }.bind(this));
-  };
+    // $http({
+    //   method: 'GET',
+    //   url: "https://api.yelp.com/v3/businesses/search?term=" + this.searchTerm + "&location=" + this.location +  "&categories=restaurants&sort_by=distance",
+    //   headers: {
+    //     'authorization': 'Bearer kxeNhjKVOho4QTBNr0mSGwdtCKGZTmW-PgvVQpjMkrNIucjw3uN63VJWg8GxX6j4DsI4Xn1oaWKQO-HzJBFQJzu0-XUzIeNZqv8ghhER5m_IQFCs3NjWF3ST6QWOWHYx',
+    //     'Access-Control-Allow-Origin' : '*',
+    //     'Access-Control-Allow-Headers' : '*',
+    //     'Access-Control-Allow-Methods' : 'OPTIONS'
+    //   },
+    // }).then(function(response) {
+    //   console.log(response.data);
+    //   console.log(response.data.places);
+    //   this.placeData = response.data.places;
+    // }.bind(this));
 
   // Function to add a recipe to the db:
   this.addRecipe = function(index) {
-
     $http({
       method: 'POST',
-      url: 'http://localhost:3000/recipes',
+      url: 'http://localhost:3000/categories/' + this.recipeGroup[index].category_id + '/recipes',
       data: { recipe: this.recipeGroup[index] }
     }).then(function(response) {
       console.log('adding recipe');
